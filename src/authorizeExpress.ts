0cddf2dbec3d35116cf2d5737dfa9eaf45729d1b
Jeremy Kirkham <jeremy@streame.com.au>
2018-10-08 18:35:32 +1100

    fix response shape for express middleware

--- src/authorizeExpress.ts
+++ src/authorizeExpress.ts
@@ -6,14 +6,23 @@ import { GraphqlAuthError } from "./exceptions/GraphqlAuthError";
  * Middleware to validate a valid JWT in the Authorization header.
  */
 export const authorizeExpress = (): express.RequestHandler =>
-  async (req, _res, next): Promise<void> => {
+  async (req, res, next): Promise<void> => {
     try {
       console.log(req.headers);
       await authorize((req.headers.Authorization as string));
+      next();
     }
     catch (e) {
       console.error(e);
-      throw new GraphqlAuthError();
+      res.status(e.statusCode != null ? e.statusCode : 500);
+      res.send(JSON.stringify({
+        data: null,
+        errors: [{
+          locations: [],
+          message: e.message,
+          path: [],
+        }],
+      }));
+      res.end();
     }
-    next();
   };
